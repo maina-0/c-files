7271f1bd7e3e6fd331e2cf9df11f28f8b115c5ee
maina-0 <maina0@proton.me>
2024-08-10 18:48:20 +0300

    1.2

--- sandbox.c
+++ sandbox.c
@@ -10,38 +10,44 @@ int main(){
     if (chdir("/")!= 0)
     {perror("error:-");
     };
-    int code = mkdir("sandboxes", 0777);
-    if (code == 0) {
+    int code0 = mkdir("sandbox", 0777);
+    if (code0 == 0) {
         printf("Directory created successfully.\n");
     } else {
         perror("Failed to create directory\n");
     }
     //moving to new dir "sandbox" now
 
-    int code1=chdir("sandboxes/");
+    int code1=chdir("sandbox/");
     if (code1 == 1){
-        perror("error moving to dir:-\n");
+        perror("error moving to dir:sandbox-\n");
     };
 
-
-    int code2=mkdir("sandbox1",0777);
-    if (code2==0){
-        printf("sandbox1 created.\n");
-    }else{
-        perror("problems creating sandbox1\n");
-    }
-    return 0;
-    int code3=system("pwd");
-    if (code3 != 0){
+    int code2=system("pwd");
+    if (code2 != 0){
         perror("err printing working dir\n");
     }return 0;
-    int code4=system("sudo dnf --installroot=/sandbox/chroot --releasever=40 groupinstall core");
+    int code4=system("dnf install \
+--installroot=/sandbox \
+--releasever=40 \
+--setopt=install_weak_deps=False \
+--setopt=keepcache=True \
+--assumeyes \
+--nodocs \
+systemd dnf glibc-langpack-en passwd rtkit policycoreutils \
+NetworkManager audit firewalld selinux-policy-targeted kbd zchunk sudo \
+vim-minimal systemd-udev rootfiles less iputils deltarpm sqlite lz4");
     if (code4!=0){
-        perror("problem installing chroot:-\n");
+        perror("problem installing chroot into sandbox dir:-\n");
     }return 0;
 
-    int code5=system("sudo chroot /sandboxes/chroot");
-    if (code5!=0){
+    int code5=mkdir("cd /sandbox/etc/", 0777);
+    int code6=system("touch resolv.conf");
+    int code7=system("touch resolv.conf");
+
+
+    int code6=system("sudo /mnt/etc/resolv.conf");
+    if (code6!=0){
         perror("problem opening chroot: \n");
         
     }return 0;
