c4eed21a98974dd80cb68a58e9bf19b77cf42ad2
maina-0 <maina0@proton.me>
2024-08-13 20:15:39 +0300

    0.2

--- makdir.c
+++ makdir.c
@@ -10,11 +10,13 @@ int main(){
 char *boy=("boy");
 printf("%s\n",boy);
 
-int code1 = mkdir("roy", 0777);
+int code1 = mkdir(boy, 0777);
 if (code1!=0){
     perror("error:-");
     }
-
-
+code1=chdir(boy);
+if (code1!=0){
+    perror("error:-");
+    }
     return 0;
     }
